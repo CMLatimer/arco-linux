ff5b060458addf0767b32a9acd52a5e6c9054abe
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2021-07-27 20:20:55 +0100

    screen size fix

--- screen_size.sh
+++ screen_size.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+xrandr --newmode "1920x1080_144.00"  452.50  1920 2088 2296 2672  1080 1083 1088 1177 -hsync +vs
+xrandr --addmod  Virtual1 1920x1080_144.00
 xrandr --output Virtual1 --mode 1920x1080_144.00
