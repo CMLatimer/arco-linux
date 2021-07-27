0f3fa3ba4b9d89ccfc2b2d6409e73c6f57f01c74
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2021-07-27 23:00:56 +0100

    made run sudo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-/home/callum/arco-linux/install_xfce4.sh
-/home/callum/arco-linux/install_lightdm.sh
-/home/callum/arco-linux/install_alacritty.sh
-/home/callum/arco-linux/install_awesomewm.sh
-/home/callum/arco-linux/install_dmenu.sh
+sudo /home/callum/arco-linux/install_xfce4.sh
+sudo /home/callum/arco-linux/install_lightdm.sh
+sudo /home/callum/arco-linux/install_alacritty.sh
+sudo /home/callum/arco-linux/install_awesomewm.sh
+sudo /home/callum/arco-linux/install_dmenu.sh
