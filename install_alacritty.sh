70a4f55505dae9ae89040952c3367753476bf1b4
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2021-07-27 17:53:14 +0100

    Alacritty Terminal and Awesome Copycats

--- install_alacritty.sh
+++ install_alacritty.sh
@@ -1,1 +1,5 @@
 pacman -S --noconfirm alacritty
+
+# Copy over alacritty.yml
+
+cp ~/arco-linux/config/alacritty.yml ~/.config/alacritty/alacritty.yml
--- install_awesomewm.sh
+++ install_awesomewm.sh
@@ -1,3 +1,25 @@
 pacman -S --noconfirm awesome
 mkdir -p ~/.config/awesome/
 cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
+
+# Awesome Copycats
+
+git clone --depth 1 --recursive https://github.com/lcpz/awesome-copycats.git
+mv -bv awesome-copycats/* ~/.condig/awesome && rm - rf awesome-copycats
+
+cd ~/.config/awesome
+cp rc.lua.template rc.lua
+
+# Awesome Copycats Dependencies
+
+# Freedeskstop
+
+git clone https://github.com/lcpz/awesome-freedesktop.git ~/.config/awesome/freedesktop
+
+# Lain
+
+git clone https://github.com/lcpz/lain.git ~/.config/awesome/lain
+
+#Copy over rc.lua
+
+cp ~/arco-linux/config/rc.lua ~/.config/awesome/rc.lua
