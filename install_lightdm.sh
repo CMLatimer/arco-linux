18984e203cf99a4b5a6e0cba29976ce0264d2dcd
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2021-07-27 19:43:42 +0100

    Added screen_size.sh to lightdm dispaly-setup-script, added lightdm config to installer

--- install_lightdm.sh
+++ install_lightdm.sh
@@ -2,3 +2,7 @@ sudo pacman -S --noconfirm xorg-server
 sudo pacman -S --noconfirm lightdm 
 sudo pacman -S --noconfirm lightdm-gtk-greeter
 systemctl enable lightdm
+
+# copy over lightdm.conf
+
+cp /home/callum/arco-linux/config/lightdm.conf /etc/lightdm/lightdm.conf
