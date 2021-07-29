f8ac6ca3402e52869daea232d9277d9d8120e646
Callum <cm_latimer-1@yahoo.co.uk>
2021-07-29 18:26:31 +0100

    added .sh to zsh, added gitkraken

---
+++ install_gitkraken.sh
@@ -0,0 +1,2 @@
+sudo pacman -S --noconfirm gitkraken
+cp /home/callum/arco-linux/config/gitkraken_profiles /home/callum/.gitkraken/profiles
--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ sudo /home/callum/arco-linux/install_alacritty.sh
 sudo /home/callum/arco-linux/install_awesomewm.sh
 sudo /home/callum/arco-linux/install_dmenu.sh
 sudo /home/callum/arco-linux/install_sublime.sh
-sudo /home/callum/arco-linux/install_zshpowerlevel
+sudo /home/callum/arco-linux/install_zshpowerlevel.sh
+sudo /home/callum/arco-linux/install_gitkraken.sh
