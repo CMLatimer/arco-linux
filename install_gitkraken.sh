21c8a1b2ce3feb20be450738b2688822fc142c19
Callum <cm_latimer-1@yahoo.co.uk>
2021-07-29 18:54:44 +0100

    fix for gitkraken profiles

--- install_gitkraken.sh
+++ install_gitkraken.sh
@@ -1,2 +1,2 @@
 sudo pacman -S --noconfirm gitkraken
-cp /home/callum/arco-linux/config/gitkraken_profiles /home/callum/.gitkraken/profiles
+cp -r /home/callum/arco-linux/config/gitkraken_profiles /home/callum/.gitkraken/profiles
