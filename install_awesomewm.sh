1af841b0cba48e2b8715eef7bb772a8674475f9e
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2021-08-16 23:59:39 +0100

    remove awesome copycats

--- install_awesomewm.sh
+++ install_awesomewm.sh
@@ -4,24 +4,24 @@ cp /etc/xdg/awesome/rc.lua /home/callum/.config/awesome/
 
 # Awesome Copycats
 
-git clone --depth 1 --recursive https://github.com/lcpz/awesome-copycats.git
-mv -bv awesome-copycats/* /home/callum/.config/awesome && rm -rf awesome-copycats
+#git clone --depth 1 --recursive https://github.com/lcpz/awesome-copycats.git
+#mv -bv awesome-copycats/* /home/callum/.config/awesome && rm -rf awesome-copycats
 
-cd /home/callum/.config/awesome
-cp rc.lua.template rc.lua
+#cd /home/callum/.config/awesome
+#cp rc.lua.template rc.lua
 
 # Awesome Copycats Dependencies
 
 # Freedeskstop
 
-rm -rf /home/callum/.config/awesome/freedesktop
-git clone https://github.com/lcpz/awesome-freedesktop.git /home/callum/.config/awesome/freedesktop
+#rm -rf /home/callum/.config/awesome/freedesktop
+#git clone https://github.com/lcpz/awesome-freedesktop.git /home/callum/.config/awesome/freedesktop
 
 # Lain
 
-rm -rf /home/callum/.config/awesome/lain
-git clone https://github.com/lcpz/lain.git /home/callum/.config/awesome/lain
+#rm -rf /home/callum/.config/awesome/lain
+#git clone https://github.com/lcpz/lain.git /home/callum/.config/awesome/lain
 
 #Copy over rc.lua
 
-cp /home/callum/arco-linux/config/rc.lua /home/callum/.config/awesome/rc.lua
+#cp /home/callum/arco-linux/config/rc.lua /home/callum/.config/awesome/rc.lua
